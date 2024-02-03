5a07f6cab7c166ddbf6554da942879c848b3eec9
ryansyrl <m.ridwan.riansyah@gmail.com>
2024-02-03 20:38:52 +0700

    feat: user can now see breadcrumb and go back to home page with clicking on breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
 
 <template>
+  <div
+    v-if="route.name === 'dpu-page'"
+    class="pl-4 pt-4 text-sm breadcrumbs"
+  >
+    <ul>
+      <li>
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+      </li>
+      <li>Daftar Piutang Unit</li>
+    </ul>
+  </div>
   <RouterView />
 </template>
